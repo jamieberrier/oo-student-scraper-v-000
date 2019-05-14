fad0a1a3a19105534a578f7ba2536b834d30be1b
jamieberrier <berrier.jamie@gmail.com>
2019-05-14 16:19:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     scraped_students = []
-
+    
     index_page.css("div.student-card").each do |student|
       scraped_students.push(name: student.css("h4.student-name").text, location: student.css("p.student-location").text, profile_url: student.css("a").attribute("href").value)
     end
